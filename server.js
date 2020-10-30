19ecdfba00d45fd254b9f6f4d3944e4435e9e6d2
imran-shaikh-au6 <imranshaikh110120@gmail.com>
2020-10-30 14:31:38 +0530

    work

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.use(
         extended: false,
     })
 );
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("client/build"));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.use(express.static("client/build"));
+
 
 
 // app.use(userRoutes);
